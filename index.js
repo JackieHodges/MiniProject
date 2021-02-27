bce846bc9c1961f5ab2ff11cb4d8ce0a498ffce6
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-02-27 13:13:49 -0600

    added inquirer

--- index.js
+++ index.js
@@ -3,4 +3,31 @@
 // prompt user for bio
 // prompt user for linkedin URL
 // prompt user for github url
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
+
+// write to HTML document
 
